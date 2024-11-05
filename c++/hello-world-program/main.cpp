8b556241bf181120c9099dde4d9459d3f69124b7
jhcaluma <34289952+jhcaluma@users.noreply.github.com>
2024-11-05 09:52:57 +0800

    added c++

---
+++ c++/hello-world-program/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number1, number2;
+
+    cout<<"\nEnter the first integer: ";
+    cin<<number1;
+    cout<<"\nEnter the second integer: ";
+    cin<<number2;
+
+    cout<<"\nAnswer: "<<number1 + number2;
+}
